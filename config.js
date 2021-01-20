cc8f09d8f7157ddb109f4e32227b305921599a8f
WLegit <64414083+WLegit@users.noreply.github.com>
2021-01-20 19:29:52 +0200

    Change green hex code to white

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	prefix: ".",
 	owners: ["402490971041824768"],
 	red: "#FF360B",
-  green: "#2BFF59",
-  orange: "#FFD132",
-  blue: "#a7d6fd"
+        green: "#FFFFFF",
+        orange: "#FFD132",
+        blue: "#a7d6fd"
 };
